078b8df383de7e81965ce32029a731634a962301
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-04 12:51:07 -0400

    added ess. dir CREATOR

--- cli/DIRECTORIES.py
+++ cli/DIRECTORIES.py
@@ -1,5 +1,17 @@
 from os.path import expanduser
 from pathlib import Path
+import os
+from LOGGER import *
 
 home = str(Path.home())
 FULL_USER_DIRECTORIES = home + "\\.config\\Printlion"
+
+def CONFIG_FILE_CREATE():
+    if not os.path.exists(FULL_USER_DIRECTORIES + "\\plugins.json"):
+        os.write(FULL_USER_DIRECTORIES+"\\plugins.json")
+    else:
+        LOGS.PLUGIN_DIR_EXISTS()
+if not os.path.exists(FULL_USER_DIRECTORIES):
+    os.mkdir(FULL_USER_DIRECTORIES)
+else:
+    LOGS.DIRECTORY_EXISTS()
---
+++ cli/LOGGER.py
@@ -0,0 +1,5 @@
+class LOGS:
+    def DIRECTORY_EXISTS():
+        print("Directory Exists. Moving on")
+    def PLUGIN_DIR_EXISTS():
+        print("Plugin Directory Exists. Moving on")
