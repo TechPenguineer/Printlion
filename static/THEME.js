1102b770d3bed0dfea4e0434bda6f0c5887c8e11
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-04 16:22:08 -0400

    updated theme switch

--- static/THEME.js
+++ static/THEME.js
@@ -3,13 +3,17 @@
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
+const SWITCH = document.getElementById("theme_selector");
 
 // function to toggle between light and dark theme
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-light');
+        SWITCH.innerHTML = "Switch to Dark Theme"
     } else {
         setTheme('theme-dark');
+        SWITCH.innerHTML = "Switch to Light Theme"
+
     }
 }
 
@@ -18,8 +22,10 @@ function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-dark');
         document.getElementById('slider').checked = false;
+        
     } else {
         setTheme('theme-light');
       document.getElementById('slider').checked = true;
+      
     }
 })();
